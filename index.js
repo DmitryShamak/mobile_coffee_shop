beedd08dd1843fec039616c45fb746f6bcc0a058
DmitryShamak <hellkitedmitry@mail.ru>
2015-05-21 08:13:18 +0300

    Added reviews

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-﻿var fs = require("fs");
+﻿var request = require("request");
+var fs = require("fs");
 var express = require("express");
 var mime = require("mime");
 
 var app = express();
 
-var port = 10102;
+var port = 10103;
 
 var styleGenerator = require("./assets/js/nodeModules/styleGenerator.js");
+
+var getReviews = function(response) {
+    request("http://cofe.vitebsk.biz/reviews/", function(err, res, body) {
+        //res.setEncoding('utf8');
+        response.setHeader("Content-Type", "text/html");
+        response.end(body);
+    });
+};
+
+app.get('/getReviews', function(req, res) {
+    getReviews(res);
+});
 
 app.get("*", function (req, res, next) {
     var url = (req.url == "/") ? "/index.html" : req.url;
